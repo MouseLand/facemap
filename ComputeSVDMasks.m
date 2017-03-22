d091741bc056c00ade2c95a943d2930d81505373
carsen-stringer <carsen.stringer@gmail.com>
2017-03-22 17:15:57 +0000

    added in SVD mask computation time

--- ComputeSVDMasks.m
+++ ComputeSVDMasks.m
@@ -5,7 +5,7 @@ fileframes = handles.fileframes;
 sc         = handles.sc;
 nXc        = floor(handles.nX/sc);
 nYc        = floor(handles.nY/sc);
-rXc        = handles.rXc;  
+rXc        = handles.rXc;
 rYc        = handles.rYc;
 
 clear uMov uMot;
@@ -18,7 +18,7 @@ wmov = find(handles.svdmat(:,3));
 wmot = wmot';
 wmov = wmov';
 %%
-nt   = 500 * sc;
+nt   = 1000 * sc;
 
 k = 0;
 nf = round(fileframes(end)/nt);
@@ -29,27 +29,27 @@ while 1
     end
     fdata = reshape(fdata, nYc, nXc, size(fdata,2));
     if ~isempty(wmov)
-    for j = wmov
-        fdata0  = fdata(rYc{j+2}, rXc{j+2}, :);
-        avgframe0 = handles.avgframe(rYc{j+2}, rXc{j+2});
-        fdata0  = reshape(fdata0, [], size(fdata0,3));
-        fdata0  = bsxfun(@minus, single(fdata0), avgframe0(:));
-        [u s v] = svd(fdata0' * fdata0);
-        umov0   = fdata0 * u(:,1:min(100,size(u,2)));
-        uMov{j}    = cat(2, uMov{j}, umov0);
-    end
+        for j = wmov
+            fdata0  = fdata(rYc{j+2}, rXc{j+2}, :);
+            avgframe0 = handles.avgframe(rYc{j+2}, rXc{j+2});
+            fdata0  = reshape(fdata0, [], size(fdata0,3));
+            fdata0  = bsxfun(@minus, single(fdata0), avgframe0(:));
+            [u s v] = svd(fdata0' * fdata0);
+            umov0   = fdata0 * u(:,1:min(100,size(u,2)));
+            uMov{j}    = cat(2, uMov{j}, umov0);
+        end
     end
     if ~isempty(wmot)
-    for j = wmot
-        fdata0  = fdata(rYc{j+2}, rXc{j+2}, :);
-        avgmotion0 = handles.avgmotion(rYc{j+2}, rXc{j+2});
-        fdata0  = reshape(fdata0, [], size(fdata0,3));
-        fdata0  = abs(diff(single(fdata0),1,2));
-        fdata0  = bsxfun(@minus, single(fdata0), avgmotion0(:));
-        [u s v] = svd(fdata0' * fdata0);
-        umot0   = fdata0 * u(:,1:min(100,size(u,2)));
-        uMot{j} = cat(2, uMot{j}, umot0);
-    end
+        for j = wmot
+            fdata0  = fdata(rYc{j+2}, rXc{j+2}, :);
+            avgmotion0 = handles.avgmotion(rYc{j+2}, rXc{j+2});
+            fdata0  = reshape(fdata0, [], size(fdata0,3));
+            fdata0  = abs(diff(single(fdata0),1,2));
+            fdata0  = bsxfun(@minus, single(fdata0), avgmotion0(:));
+            [u s v] = svd(fdata0' * fdata0);
+            umot0   = fdata0 * u(:,1:min(100,size(u,2)));
+            uMot{j} = cat(2, uMot{j}, umot0);
+        end
     end
     k = k+1;
     fprintf('frameset %d/%d  time %3.2fs\n', k, round(fileframes(end)/nt), toc);
@@ -57,18 +57,20 @@ end
 
 % take SVD of SVD components
 if ~isempty(wmov)
-for j = wmov
-    [u s v] = svd(uMov{j}'*uMov{j});
-    uMovMask = uMov{j} * u(:,1:min(100,size(u,2)));
-    handles.movieMask{j} = uMovMask;
-end
+    for j = wmov
+        fprintf('computing SVD of movie of size %d time %3.2fs\n', size(uMov{j},2),toc);
+        [u s v] = svd(uMov{j}'*uMov{j});
+        uMovMask = uMov{j} * u(:,1:min(100,size(u,2)));
+        handles.movieMask{j} = uMovMask;
+    end
 end
 if ~isempty(wmot)
-for j = wmot
-    [u s v] = svd(uMot{j}'*uMot{j});
-    uMotMask = uMot{j} * u(:,1:min(100,size(u,2)));
-    uMotMask = normc(uMotMask);
-    handles.motionMask{j} = uMotMask;
-end
+    for j = wmot
+        fprintf('computing SVD of motion of size %d time %3.2fs\n', size(uMov{j},2),toc);
+        [u s v] = svd(uMot{j}'*uMot{j});
+        uMotMask = uMot{j} * u(:,1:min(100,size(u,2)));
+        uMotMask = normc(uMotMask);
+        handles.motionMask{j} = uMotMask;
+    end
 end
 fclose('all');
