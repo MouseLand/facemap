5942a0f4ca5915c71b615b8fb5bfe44e8b8705a1
carsen <carsen.stringer@gmail.com>
2019-09-23 13:29:40 -0400

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="facemap",
-    version="0.1.3",
+    version="0.1.4",
     author="Carsen Stringer",
     author_email="carsen.stringer@gmail.com",
     description="Processing motion SVDs of videos",
