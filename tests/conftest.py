aec005baf1c2325f0fec21ed3011dad8f296b16c
Atika-Syeda <syeda.atika@ucalgary.ca>
2020-10-23 16:08:45 -0400

    testing added

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+import os, sys
+from pathlib import Path
+
+@pytest.fixture()
+def video_names():
+    video1_names = ['cam1_G7c1_1.avi']
+    video2_names = ['cam1_G7c1_2.avi']
+    return video1_names, video2_names
+
+@pytest.fixture()
+def data_dir(video_names):
+    data_dir1 = os.path.join(os.getcwd(),'Video_samples/sample_movies2/')
+    data_dir2 = os.path.join(os.getcwd(),'Video_samples/sample_movies/')
+    return data_dir1, data_dir2
