0f45bda61fe93181c7937750d2da14eed6b4ff1f
Carsen Stringer <carsen.stringer@gmail.com>
2022-10-03 11:53:38 -0400

    fixing setup removal of torch dependency

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ install_deps = [
 try:
     import torch
     a = torch.ones(2, 3)
-    version = int(torch.__version__[2])
+    version = int(torch.__version__.split('.')[1])
     if version >= 9:
         install_deps.remove('torch>=1.9')
 except:
