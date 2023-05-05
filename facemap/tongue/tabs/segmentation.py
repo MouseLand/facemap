0f0a11ea7f9410310fa75d4b8271db3f94bcf8c7
Atika-Syeda <syeda.atika@ucalgary.ca>
2023-05-05 13:17:50 -0400

    Add video player

--- facemap/tongue/tabs/segmentation.py
+++ facemap/tongue/tabs/segmentation.py
@@ -1,5 +1,8 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from ..tabs.videoplayer import VideoPlayer
 
 class SegmentationTab(QWidget):
     def __init__(self):
@@ -20,8 +23,8 @@ class SegmentationTab(QWidget):
         # Set up the splitter
         splitter = QSplitter(Qt.Horizontal)
         # Set up the left panel with buttons
-        button_widget = QWidget()
-        button_layout = QVBoxLayout(button_widget)
+        button_panel = QWidget()
+        button_layout = QVBoxLayout(button_panel)
         button_layout.setContentsMargins(0, 0, 0, 0)
 
         button_style = """
@@ -49,8 +52,6 @@ class SegmentationTab(QWidget):
         add_video_button.setStyleSheet(button_style)
         button_layout.addWidget(add_video_button)
 
-        # 
-
         # Add number of videos loaded label
         self.num_videos_label = QLabel("0 Videos Loaded")
         self.num_videos_label.setStyleSheet("color: lightgrey;")
@@ -65,15 +66,15 @@ class SegmentationTab(QWidget):
 
         # Add radio buttons for video views
         self.video_view_label = QLabel("Video View:")
+        self.video_view_label.setAlignment(Qt.AlignCenter)
         self.video_view_label.setStyleSheet("color: lightgrey;")
         button_layout.addWidget(self.video_view_label)
-
         
         video_view_groupbox = QGroupBox()
         # change style sheet to remove border
         video_view_groupbox.setStyleSheet("border: none;")
         video_view_groupbox.setLayout(QHBoxLayout())
-        self.video_view_group = QButtonGroup(button_widget)
+        self.video_view_group = QButtonGroup(button_panel)
 
         bottom_view_button = QRadioButton("Bottom")
         bottom_view_button.setStyleSheet("color: white;")
@@ -91,26 +92,31 @@ class SegmentationTab(QWidget):
         video_view_groupbox.layout().addWidget(other_view_button)
 
         button_layout.addWidget(video_view_groupbox)
-        
-        show_filenames_button = QPushButton("Get loaded videos")
-        show_filenames_button.setStyleSheet("background-color: rgb(196, 108, 57); color: white;")
-        show_filenames_button.clicked.connect(self.show_video_filenames)
-        show_filenames_button.setStyleSheet(button_style)
-        button_layout.addWidget(show_filenames_button)
 
+        """
         # Set up the right panel with loaded video label
-        video_label_widget = QWidget()
-        video_label_layout = QVBoxLayout(video_label_widget)
-        video_label_layout.setContentsMargins(0, 0, 0, 0)
+        video_panel = QWidget()
+        video_panel_layout = QVBoxLayout(video_panel)
 
-        loaded_videos_label = QLabel("No Videos Loaded")
-        loaded_videos_label.setStyleSheet("color: lightgrey;")
-        loaded_videos_label.setAlignment(Qt.AlignCenter)
-        video_label_layout.addWidget(loaded_videos_label)
+        self.media_player = QMediaPlayer(self)
+        self.video_widget = QVideoWidget(self)
 
+        self.video_slider = QSlider(Qt.Horizontal)
+        self.video_slider.setEnabled(False)
+        self.video_slider.sliderMoved.connect(self.media_player.setPosition)        
+        self.media_player.positionChanged.connect(self.update_slider_position)
+        self.media_player.durationChanged.connect(self.video_slider.setMaximum)
+
+        video_panel_layout.addWidget(self.video_widget)
+        video_panel_layout.addWidget(self.video_slider)
+        video_panel_layout.addStretch()
+
+        # Set the size policy of the video panel
+        video_panel.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        """
         # Add the panels to the splitter
-        splitter.addWidget(button_widget)
-        splitter.addWidget(video_label_widget)
+        splitter.addWidget(button_panel)
+        splitter.addWidget(VideoPlayer())
         splitter.setSizes([100, 500])
 
         # Set the style sheet for the dark theme and use white text
@@ -126,6 +132,30 @@ class SegmentationTab(QWidget):
 
         # Add the splitter to the layout
         self.layout.addWidget(splitter, 0, 0)
+
+    def play_video(self, video_file):
+        # Set the media player's media and set the video output to the video widget
+        self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(video_file)))
+        self.media_player.setVideoOutput(self.video_widget)
+
+        # Add the video widget to the layout
+        self.layout.addWidget(self.video_widget, 0, 1)
+
+        # Enable the slider and set its maximum value to the media player's duration
+        self.video_slider.setEnabled(True)
+        self.video_slider.setMaximum(self.media_player.duration())
+
+        # Play the video
+        self.media_player.play()
+
+    def show_video_filenames(self):
+        # Show a message box with the list of video filenames
+        if self.video_filenames:
+            message = "\n".join(self.video_filenames)
+        else:
+            message = "No Videos Loaded"
+        QMessageBox.information(self, "Loaded Videos", message)
+
 
     def add_video(self):
         # Show file dialog to select video files
@@ -144,11 +174,9 @@ class SegmentationTab(QWidget):
                 loaded_videos_text = f"{num_videos} Videos Loaded"
             self.num_videos_label.setText(loaded_videos_text)
 
-    def show_video_filenames(self):
-        # Show a message box with the list of video filenames
-        if self.video_filenames:
-            message = "\n".join(self.video_filenames)
-        else:
-            message = "No Videos Loaded"
-        QMessageBox.information(self, "Loaded Videos", message)
+            # Play the first video in the list
+            if num_videos == 1:
+                self.play_video(self.video_filenames[0])
 
+    def update_slider_position(self, position):
+        self.video_slider.setValue(position)
---
+++ facemap/tongue/tabs/videoplayer.py
@@ -0,0 +1,100 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import  QSize, QUrl, Qt
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+# Import QFont
+from PyQt5.QtGui import QFont, QIcon
+
+class VideoPlayer(QWidget):
+
+    def __init__(self, parent=None):
+        super(VideoPlayer, self).__init__(parent)
+
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+
+        btnSize = QSize(16, 16)
+        videoWidget = QVideoWidget()
+
+        openButton = QPushButton("Open Video")   
+        openButton.setToolTip("Open Video File")
+        openButton.setStatusTip("Open Video File")
+        openButton.setFixedHeight(24)
+        openButton.setIconSize(btnSize)
+        openButton.setFont(QFont("Noto Sans", 8))
+        openButton.setIcon(QIcon.fromTheme("document-open", QIcon("D:/_Qt/img/open.png")))
+        openButton.clicked.connect(self.abrir)
+
+        self.playButton = QPushButton()
+        self.playButton.setEnabled(False)
+        self.playButton.setFixedHeight(24)
+        self.playButton.setIconSize(btnSize)
+        self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.playButton.clicked.connect(self.play)
+
+        self.positionSlider = QSlider(Qt.Horizontal)
+        self.positionSlider.setRange(0, 0)
+        self.positionSlider.sliderMoved.connect(self.setPosition)
+
+        self.statusBar = QStatusBar()
+        self.statusBar.setFont(QFont("Arial", 10))
+        # Set text color to white
+        self.statusBar.setStyleSheet("color: white;")
+        self.statusBar.setFixedHeight(14)
+
+        controlLayout = QHBoxLayout()
+        controlLayout.setContentsMargins(0, 0, 0, 0)
+        controlLayout.addWidget(openButton)
+        controlLayout.addWidget(self.playButton)
+        controlLayout.addWidget(self.positionSlider)
+
+        layout = QVBoxLayout()
+        layout.addWidget(videoWidget)
+        layout.addLayout(controlLayout)
+        layout.addWidget(self.statusBar)
+
+        self.setLayout(layout)
+
+        self.mediaPlayer.setVideoOutput(videoWidget)
+        self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
+        self.mediaPlayer.positionChanged.connect(self.positionChanged)
+        self.mediaPlayer.durationChanged.connect(self.durationChanged)
+        self.mediaPlayer.error.connect(self.handleError)
+        self.statusBar.showMessage("Ready")
+
+    def abrir(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Selecciona los mediose",
+                ".", "Video Files (*.mp4 *.flv *.ts *.mts *.avi)")
+
+        if fileName != '':
+            self.mediaPlayer.setMedia(
+                    QMediaContent(QUrl.fromLocalFile(fileName)))
+            self.playButton.setEnabled(True)
+            self.statusBar.showMessage(fileName)
+            self.play()
+
+    def play(self):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.mediaPlayer.pause()
+        else:
+            self.mediaPlayer.play()
+
+    def mediaStateChanged(self, state):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.playButton.setIcon(
+                    self.style().standardIcon(QStyle.SP_MediaPause))
+        else:
+            self.playButton.setIcon(
+                    self.style().standardIcon(QStyle.SP_MediaPlay))
+
+    def positionChanged(self, position):
+        self.positionSlider.setValue(position)
+
+    def durationChanged(self, duration):
+        self.positionSlider.setRange(0, duration)
+
+    def setPosition(self, position):
+        self.mediaPlayer.setPosition(position)
+
+    def handleError(self):
+        self.playButton.setEnabled(False)
+        self.statusBar.showMessage("Error: " + self.mediaPlayer.errorString())
