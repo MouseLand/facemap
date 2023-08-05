a6b12aad94e3eca4ebc1527093549e2a837deb46
Atika-Syeda <syeda.atika@ucalgary.ca>
2023-08-05 04:31:35 -0400

    Minor updates

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2023, Carsen Stringer & Atika Syeda"
 author = "Carsen Stringer & Atika Syeda"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.1"
+release = "1.0.2"
 
 
 # -- General configuration ---------------------------------------------------
