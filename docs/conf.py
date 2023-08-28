208d53f38f378267869b4ab586904536d5b087ba
Atika-Syeda <syeda.atika@ucalgary.ca>
2023-08-28 14:38:15 -0400

    Update readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -73,7 +73,7 @@ html_favicon = "_static/favicon.ico"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-"""
+
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = ["_themes"]
 html_theme_options = {
@@ -91,7 +91,7 @@ html_theme_options = {
     'includehidden': True,
     'titles_only': False
 }
-"""
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
