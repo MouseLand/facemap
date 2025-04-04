2c12b4b09c20d93d35ea5c5f70fb9fc0d95f8460
unknown <carsen.stringer@gmail.com>
2025-04-04 15:19:18 -0400

    removing import test

--- tests/test_import.py
+++ tests/test_import.py
@@ -9,19 +9,18 @@ def test_facemap_imports():
     from facemap import process, pupil, running, utils
 
 
-def test_neural_prediction_imports():
-    from facemap.neural_prediction import (
-        neural_activity,
-        neural_model,
-        prediction_utils,
-    )
+# def test_neural_prediction_imports():
+#     from facemap.neural_prediction import (
+#         neural_activity,
+#         neural_model,
+#         prediction_utils,
+#     )
 
 
 def test_pose_imports():
     from facemap.pose import (
         facemap_network,
         model_loader,
-        model_training,
         pose,
         pose_helper_functions,
         transforms,
