df6c55e3f8c4a691141b7afbebb90ecb83ba422e
Yaroslav Halchenko <debian@onerussian.com>
2025-11-17 14:09:55 -0800

    pragma to ignore mot

--- facemap/process.py
+++ facemap/process.py
@@ -710,7 +710,7 @@ def run(
     Lybin, Lxbin, iinds = binned_inds(Ly, Lx, sbin)
     LYbin, LXbin, sybin, sxbin = utils.video_placement(Lybin, Lxbin)
 
-    # number of mot/mov ROIs
+    # number of mot/mov ROIs  # codespell:ignore
     nroi = 0
     if rois is not None:
         for r in rois:
